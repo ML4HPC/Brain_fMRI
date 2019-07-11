6943a261fc1bc29170b86e3a3cbfcfcaf40aea46
Seungwook Han <sh3264@columbia.edu>
2019-07-11 15:05:03 -0400

    Fixed forward

--- resnet3d.py
+++ resnet3d.py
@@ -206,9 +206,10 @@ class PipelinedResNet3d(ResNet3d):
         x = self.relu(x)
         x = self.maxpool(x)
         x = self.layer1(x)
-        x = self.layer2(x)
 
         x = x.to(self.dev2)
+        
+        x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
         x = self.avgpool(x)

1fe554b3ace28732b31dd085529a327969b3c184
Seungwook Han <sh3264@columbia.edu>
2019-07-29 13:36:05 -0400

    Added resize into preprocessing

--- readimage.py
+++ readimage.py
@@ -1,6 +1,8 @@
 import os
 import nibabel as nib
 import numpy as np
+import argparse
+from scipy.ndimage import zoom
 
 path = '/global/cscratch1/sd/yanzhang/data_brain/image03/'
 train = 'training/'
@@ -9,7 +11,7 @@ test = 'testing/'
 lattername = '/baseline/structural/t1_brain.nii.gz'
 
 
-def readimages(path, data_for, lattername):
+def readimages(path, data_for, lattername, resize):
     filenames = os.listdir(path+data_for)
     images = {}
     i = 0
@@ -20,22 +22,35 @@ def readimages(path, data_for, lattername):
         #print(name)
         full_path = path+data_for+name+lattername
         img = nib.load(full_path)
+
+        # Resize, if necessary
+        img_data = np.array(img.dataobj)
+        if resize:
+            img_data = zoom(img_data, resize)
+            
+        resized_img = nib.Nifti1Image(img_data, img.affine, img.header)
+
         images[name] = img
 #        print(img)
     return images
 
-print('processing training!')
-train_img = readimages(path, train, lattername)
-print('processing valid!')
-valid_img = readimages(path, valid, lattername)
-print('processing test!')
-test_img = readimages(path, test, lattername)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Read and process images')
+    parser.add_argument('--resize', type=float)
+    args = parser.parse_args()
+
+    print('processing training!')
+    train_img = readimages(path, train, lattername, args.resize)
+    print('processing valid!')
+    valid_img = readimages(path, valid, lattername, args.resize)
+    print('processing test!')
+    test_img = readimages(path, test, lattername, args.resize)
 
 
-print('saving images!')
-np.save('train_img.npy', train_img)
-np.save('valid_img.npy', valid_img)
-np.save('test_img.npy', test_img)
-print('done saving!')
+    print('saving images!')
+    np.save('train_img.npy', train_img)
+    np.save('valid_img.npy', valid_img)
+    np.save('test_img.npy', test_img)
+    print('done saving!')
 
 

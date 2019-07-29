dad553473fd7a04213574cd3543948cdc9c0ce8b
Seungwook Han <sh3264@columbia.edu>
2019-07-29 13:41:41 -0400

    Added argparse

--- readimage.py
+++ readimage.py
@@ -4,7 +4,7 @@ import numpy as np
 import argparse
 from scipy.ndimage import zoom
 
-path = '/global/cscratch1/sd/yanzhang/data_brain/image03/'
+#path = '/global/cscratch1/sd/yanzhang/data_brain/image03/'
 train = 'training/'
 valid = 'validation/'
 test = 'testing/'
@@ -27,7 +27,7 @@ def readimages(path, data_for, lattername, resize):
         img_data = np.array(img.dataobj)
         if resize:
             img_data = zoom(img_data, resize)
-            
+
         resized_img = nib.Nifti1Image(img_data, img.affine, img.header)
 
         images[name] = img
@@ -37,20 +37,27 @@ def readimages(path, data_for, lattername, resize):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Read and process images')
     parser.add_argument('--resize', type=float)
+    parser.add_argument('--data_dir', help='Path to dataset images')
+    parser.add_argument('--output_dir', help='Path to directory for saving outputs')
     args = parser.parse_args()
 
+    try:
+        os.mkdir(args.output_dir)
+    except:
+        raise Exception('Could not create output directory')
+
     print('processing training!')
-    train_img = readimages(path, train, lattername, args.resize)
+    train_img = readimages(args.data_dir, train, lattername, args.resize)
     print('processing valid!')
-    valid_img = readimages(path, valid, lattername, args.resize)
+    valid_img = readimages(args.data_dir, valid, lattername, args.resize)
     print('processing test!')
-    test_img = readimages(path, test, lattername, args.resize)
+    test_img = readimages(args.data_dir, test, lattername, args.resize)
 
 
     print('saving images!')
-    np.save('train_img.npy', train_img)
-    np.save('valid_img.npy', valid_img)
-    np.save('test_img.npy', test_img)
+    np.save(os.path.join(args.output_dir, 'train_img.npy'), train_img)
+    np.save(os.path.join(args.output_dir, 'valid_img.npy'), valid_img)
+    np.save(os.path.join(args.output_dir, 'test_img.npy'), test_img)
     print('done saving!')
 
 

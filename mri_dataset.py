0ee58d141adca0f923dba5dd57b825472f20bd75
Seungwook Han <sh3264@columbia.edu>
2019-07-30 02:03:15 -0400

    Changing interpolation with np

--- mri_dataset.py
+++ mri_dataset.py
@@ -15,8 +15,13 @@ class MRIDataset(Dataset):
         return len(self.Y_data)
         
     def __getitem__(self, idx):
-        img_data = np.array(self.X_data[idx].dataobj)
-        if self.resize:
-            img_data = zoom(img_data, self.resize)
+        # img_data = np.array(self.X_data[idx].dataobj)
+        # if self.resize:
+        #     img_data = zoom(img_data, self.resize)
+        dim = 90
+        x = np.array(self.X_data[idx].dataobj)
+        x = np.resize(x, (dim, dim, dim))
+    
+        return (x, self.Y_data[idx])
 
-        return (img_data, self.Y_data[idx])
+        # return (img_data, self.Y_data[idx])

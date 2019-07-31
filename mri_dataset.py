dbf5c7c299066c4584e6d08228dc87f8fb8bb4da
Seungwook Han <sh3264@columbia.edu>
2019-07-31 15:48:18 -0400

    Compatible for resize or non-resize

--- mri_dataset.py
+++ mri_dataset.py
@@ -6,10 +6,7 @@ class MRIDataset(Dataset):
     def __init__(self, input_data, target, resize):
         self.X_data = input_data
         self.Y_data = target
-        if resize:
-            self.resize = (resize, resize, resize)
-        else:
-            self.resize = 0
+        self.resize = resize
     
     def __len__(self):
         return len(self.Y_data)
@@ -18,9 +15,11 @@ class MRIDataset(Dataset):
         # img_data = np.array(self.X_data[idx].dataobj)
         # if self.resize:
         #     img_data = zoom(img_data, self.resize)
-        dim = 90
+        #dim = 90
         x = np.array(self.X_data[idx].dataobj)
-        x = np.resize(x, (dim, dim, dim))
+        
+        if self.resize > 0:
+            x = np.resize(x, (self.resize, self.resize, self.resize))
     
         return (x, self.Y_data[idx])
 

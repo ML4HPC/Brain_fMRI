e69d280f642442e186b6003da5afea323ce3f108
Seungwook Han <sh3264@columbia.edu>
2019-08-01 11:50:26 -0400

    Eval cnn

---
+++ eval_cnn_3d.py
@@ -0,0 +1,63 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import numpy as np
+from mri_dataset import MRIDataset
+from model_3d import CNN, train, eval
+import argparse
+import os
+
+if __name__ == "__main__":
+    torch.cuda.set_device(0)
+
+    # Parsing arguments
+    parser = argparse.ArgumentParser(description='3D CNN for regression')
+    parser.add_argument('--data_dir')
+    parser.add_argument('--output_dir')
+    parser.add_argument('--valid_batch_size', type=int, default=4)
+    parser.add_argument('--checkpoint_state', default='')
+    parser.add_argument('--checkpoint_epoch', type=int, default=0)
+    parser.add_argument('--resize', type=int, default=0)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--momentum', type=float, default=0.5)
+    parser.add_argument('--optimizer', default='sgd', help='Optimizer type: adam, sgd')
+    args = parser.parse_args()
+
+    # Setting device
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = CNN()
+    model.cuda()
+
+    # Setting up data parallelism, if available
+    if torch.cuda.device_count() > 1:
+        print('Using Data Parallelism with multiple GPUs available')
+        model = nn.DataParallel(model)
+
+    # Load from checkpoint, if available
+    if args.checkpoint_state:
+        saved_state = torch.load(args.checkpoint_state)
+        model.load_state_dict(saved_state)
+        print('Loaded model from checkpoint')
+
+    # Load and create datasets
+    train_img = np.load(os.path.join(args.data_dir, 'train_data_img.npy'), allow_pickle=True)
+    valid_img = np.load(os.path.join(args.data_dir, 'valid_data_img.npy'), allow_pickle=True)
+    train_target = np.load(os.path.join(args.data_dir, 'train_data_target.npy'), allow_pickle=True)
+    valid_target = np.load(os.path.join(args.data_dir, 'valid_data_target.npy'), allow_pickle=True)
+
+    train_dataset = MRIDataset(train_img, train_target, args.resize)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.train_batch_size)
+    valid_dataset = MRIDataset(valid_img, valid_target, args.resize)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.valid_batch_size)
+
+
+    if args.optimizer == 'sgd':
+        optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    elif args.optimizer == 'adam':
+        optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.001)
+    
+    loss = nn.L1Loss()
+    cur_mse = eval(model, valid_loader, loss)
+    print('MSE Evaluation: {}'.format(cur_mse))
+
+    

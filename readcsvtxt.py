cf337742357020735d039aa132610d929f367c79
Seungwook Han <sh3264@columbia.edu>
2019-08-30 12:26:15 -0400

    High edu taking to 777 or 999 into account

--- readcsvtxt.py
+++ readcsvtxt.py
@@ -20,7 +20,20 @@ def get_covariates(key, acspsw_data, abcd_data, pdemo_data):
     age = int(acspsw_row['interview_age'])
     gender = 0 if (acspsw_row['gender'] == 'M').bool() else 1
     race_ethnicity = int(acspsw_row['race_ethnicity'])
-    high_edu = max(int(pdemo_row['demo_prnt_ed_v2']) if not pdemo_row['demo_prnt_ed_v2'].isnull().bool() else 0, int(pdemo_row['demo_prtnr_ed_v2']) if not pdemo_row['demo_prtnr_ed_v2'].isnull().bool() else 0)
+
+    high_edu = -1
+    high_edu_prnt1 = int(pdemo_row['demo_prnt_ed_v2']) if not pdemo_row['demo_prnt_ed_v2'].isnull().bool() else -1
+    high_edu_prnt2 = int(pdemo_row['demo_prtnr_ed_v2']) if not pdemo_row['demo_prtnr_ed_v2'].isnull().bool() else -1
+    
+    if (high_edu_prnt1 == 777 or high_edu_prnt1 == 999) and (high_edu_prnt2 == 777 or high_edu_prnt2 == 999):
+        high_edu = -1
+    elif high_edu_prnt1 == 777 or high_edu_prnt1 == 999:
+        high_edu = high_edu_prnt2
+    elif high_edu_prnt2 == 777 or high_edu_prnt2 == 999:
+        high_edu = high_edu_prnt1
+    else:
+        high_edu = max(high_edu_prnt1, high_edu_prnt2)
+    
     married = int(pdemo_row['demo_prnt_marital_v2'])
     site = int(abcd_row['site_id_l'].str.strip('site'))
 

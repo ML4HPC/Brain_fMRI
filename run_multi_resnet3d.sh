6b4a81e8fa2da1dfa6af18b196ef93853143144a
Seungwook Han <sh3264@columbia.edu>
2019-09-16 22:17:02 +0000

    Saving script file for each run to save configs

--- run_multi_resnet3d.sh
+++ run_multi_resnet3d.sh
@@ -8,5 +8,10 @@ fi
 
 bsz=4
 outdir=$1
+cwd=$(pwd)
+cur_filepath="${cwd}/${0}"
 rm -rf $outdir
-python3 run_multi_resnet3d.py --data_dir=../data_3d/ --output_dir=$outdir --train_batch_size=$bsz --valid_batch_size=4 --epoch=7 --optimizer=sgd --normalize==True --log=True
+python3 run_multi_resnet3d.py --data_dir=../data_3d/ --output_dir=$outdir --train_batch_size=$bsz --valid_batch_size=4 --epoch=10 --optimizer=sgd --normalize==True --log=True --lr=0.001
+cp $cur_filepath $outdir 
+
+

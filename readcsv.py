f05004ac925dcf3b5e95f19b0ac39a9d5f6528bb
Seungwook Han <sh3264@columbia.edu>
2019-09-23 14:09:49 -0400

    Fixed test save

--- readcsv.py
+++ readcsv.py
@@ -47,4 +47,4 @@ if __name__ == "__main__":
     print('saving valid dict!')
     np.save(os.path.join(args.output_dir, 'csv_valid_target.npy'), csv_valid)
     print('saving test dict!')
-    np.save(os.path.join(args.output_dir, 'csv_test_target.npy'), csv_valid)
+    np.save(os.path.join(args.output_dir, 'csv_test_target.npy'), csv_test)

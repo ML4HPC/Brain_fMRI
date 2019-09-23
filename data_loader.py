7c7ae8967a15daeaebb314ba7fddd69a11e05c72
Seungwook Han <sh3264@columbia.edu>
2019-09-23 10:45:16 -0400

    Including test data for processing

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,7 @@ import numpy as np
 
 train_target = np.load('csv_train_target.npy')
 valid_target = np.load('csv_valid_target.npy')
+test_target = np.load('csv_test_target.npy')
 
 train_img = np.load('train_img.npy')
 valid_img = np.load('valid_img.npy')
@@ -9,8 +10,10 @@ test_img = np.load('test_img.npy')
 
 train_data_img = []
 valid_data_img = []
+test_data_img = []
 train_data_target = []
 valid_data_target = []
+test_data_target = []
 
 
 #for i in range(len(train_target.items())):
@@ -35,6 +38,11 @@ for key in valid_target.item().keys():
         #print(valid_data[key])
         #print(valid_data[key][0].shape)
 
+for key in test_target.item().keys():
+    if key in test_img.item().keys():
+           test_data_img.append(test_img.item()[key])
+           test_data_target.append(np.float(test_target.item()[key]))
+
 '''
 for i in range(len(train_data_target)):
     train_data_img[i] = np.array(train_data_img[i].dataobj)
@@ -49,8 +57,10 @@ for i in range(len(valid_data_target)):
 
 np.save('train_data_img.npy', train_data_img)
 np.save('valid_data_img.npy', valid_data_img)
+np.save('test_data_img.npy', valid_data_img)
 np.save('train_data_target.npy', train_data_target)
 np.save('valid_data_target.npy', valid_data_target)
+np.save('test_data_target.npy', valid_data_target)
 
 
 

0a071270199174ace4ac88953cae9703567432bd
Seungwook Han <sh3264@columbia.edu>
2019-09-23 10:28:40 -0400

    Adjusting structural MRI preprocessing to use dti-compatible subjects only

--- readcsv.py
+++ readcsv.py
@@ -18,12 +18,15 @@ if __name__ == "__main__":
     parser.add_argument('--output_dir', help='Path to directory for saving outputs')
     args = parser.parse_args()
 
-    train = "training_fluid_intelligenceV1.csv"
-    valid = "validation_fluid_intelligenceV1.csv"
+    # Using residualized fluid intelligence scores using our own brain volumetric variable
+    train = "training_fluid_intelligence_sri.csv"
+    valid = "validation_fluid_intelligence_sri.csv"
+    test = "test_fluid_intelligence_sri.csv"
     
     csv_train = readcsv(args.data_dir, train)
     csv_valid = readcsv(args.data_dir, valid)
-
+    csv_test = readcsv(args.data_dir, test)
+    
     for key, value in csv_train.items():
         print(key, value)
 
@@ -34,3 +37,5 @@ if __name__ == "__main__":
     np.save(os.path.join(args.data_dir, 'csv_train_target.npy'), csv_train)
     print('saving valid dict!')
     np.save(os.path.join(args.data_dir, 'csv_valid_target.npy'), csv_valid)
+    print('saving test dict!')
+    np.save(os.path.join(args.data_dir, 'csv_test_target.npy'), csv_valid)

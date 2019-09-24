4b9bb31ecf101f5c5b6948d02036875c29cda3ad
Seungwook Han <sh3264@columbia.edu>
2019-09-24 12:10:49 -0400

    Added more imports

--- multi_input_resnet3d.py
+++ multi_input_resnet3d.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from resnet3d import Bottleneck3d
+from resnet3d import Bottleneck3d, conv1x1_3d, conv3x3_3d
 
 class ResNet3dPre(nn.Module):
     """ 

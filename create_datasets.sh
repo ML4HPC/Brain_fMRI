72b3cb33675081ffb80d0c0758f2b755df07577b
Seungwook Han <sh3264@columbia.edu>
2019-10-06 17:29:28 -0400

    Split btw structural and dti

--- create_datasets.sh
+++ create_datasets.sh
@@ -5,10 +5,18 @@ struct_data_dir="/home/seungwook/data/data_T1_T2_201909/"
 dti_data_dir="/home/seungwook/data/data_DTI_tensor/"
 output_dir="/home/seungwook/data/processed/"
 
-for mri_type in T1 T2 MD FA AD RD
+for mri_type in T1 T2
 do
     python3 readcsv.py --data_dir=$csv_data_dir --output_dir=$output_dir
     python3 readimage.py --data_dir=$struct_data_dir --output_dir=$output_dir --mri_type=$mri_type
     python3 data_loader.py --data_dir=$output_dir --output_dir=$output_dir --mri_type=$mri_type
 done
+
+for mri_type in MD FA AD RD
+do
+    python3 readcsv.py --data_dir=$csv_data_dir --output_dir=$output_dir
+    python3 readimage.py --data_dir=$dti_data_dir --output_dir=$output_dir --mri_type=$mri_type
+    python3 data_loader.py --data_dir=$output_dir --output_dir=$output_dir --mri_type=$mri_type
+done
+
 

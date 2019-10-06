a632ebb0a9a4a8bf5e20ed7c2d48486ceef59a3e
Seungwook Han <sh3264@columbia.edu>
2019-10-06 17:33:58 -0400

    Echoing mri type

--- create_datasets.sh
+++ create_datasets.sh
@@ -7,6 +7,7 @@ output_dir="/home/seungwook/data/processed/"
 
 for mri_type in T1 T2
 do
+    echo $mri_type
     python3 readcsv.py --data_dir=$csv_data_dir --output_dir=$output_dir
     python3 readimage.py --data_dir=$struct_data_dir --output_dir=$output_dir --mri_type=$mri_type
     python3 data_loader.py --data_dir=$output_dir --output_dir=$output_dir --mri_type=$mri_type
@@ -14,6 +15,7 @@ done
 
 for mri_type in MD FA AD RD
 do
+    echo $mri_type
     python3 readcsv.py --data_dir=$csv_data_dir --output_dir=$output_dir
     python3 readimage.py --data_dir=$dti_data_dir --output_dir=$output_dir --mri_type=$mri_type
     python3 data_loader.py --data_dir=$output_dir --output_dir=$output_dir --mri_type=$mri_type

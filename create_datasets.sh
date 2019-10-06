107b2f17d8372647c3dc6470c1f856d1f8c8f9f5
Seungwook Han <sh3264@columbia.edu>
2019-10-06 17:05:35 -0400

    Script for creating dataset

---
+++ create_datasets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+struct_data_dir="/home/seungwook/data/data_T1_T2_201909"
+dti_data_dir="/home/seungwook/data/data_DTI_tensor"
+output_dir="/home/seungwook/data/processed/"
+mri_type="T1"
+
+python3 readcsv.py --data_dir=$struct_data_dir --output_dir=$output_dir
+python3 readimage.py --data_dir=$struct_data_dir --output_dir=$output_dir --mri_type=$mri_type
+python3 data_loader.py --data_dir=$struct_data_dir --output_dir=$output_dir --mri_type=$mri_type
+

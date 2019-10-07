27c00b4f0351723b071e06976e23ccffe046e6a2
Seungwook Han <sh3264@columbia.edu>
2019-10-06 23:53:25 -0400

    Calculating means & stds for normalization

--- calc_norm_val.py
+++ calc_norm_val.py
@@ -17,22 +17,53 @@ if __name__ == "__main__":
 
 
     # Load and create datasets
-    train_img = np.load(os.path.join(args.data_dir, 'train_data_img.npy'), allow_pickle=True)
-    train_target = np.load(os.path.join(args.data_dir, 'train_data_target.npy'), allow_pickle=True)
-    train_dataset = MRIDataset(train_img, train_target, args.resize)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.train_batch_size)
-    
-    mean = 0.0
-    for batch_img, _ in train_loader:
-        mean += torch.sum(batch_img)
-    
-    mean /= len(train_loader.dataset) * 120 * 120 * 120
+    train_img_T1 = np.load(os.path.join(args.data_dir, 'train_data_img_T1.npy'), allow_pickle=True)
+    train_img_T2 = np.load(os.path.join(args.data_dir, 'train_data_img_T2.npy'), allow_pickle=True)
+    train_img_FA = np.load(os.path.join(args.data_dir, 'train_data_img_FA.npy'), allow_pickle=True)
+    train_img_MD = np.load(os.path.join(args.data_dir, 'train_data_img_MD.npy'), allow_pickle=True)
+    train_img_AD = np.load(os.path.join(args.data_dir, 'train_data_img_AD.npy'), allow_pickle=True)
+    train_img_RD = np.load(os.path.join(args.data_dir, 'train_data_img_RD.npy'), allow_pickle=True)
 
-    var = 0.0
-    for batch_img, _ in train_loader:
-        var += torch.sum(np.power(np.subtract(batch_img, mean), 2))
-    
-    var /= len(train_loader.dataset) * 120 * 120 * 120
-    std = np.sqrt(var)
+    train_dataset_T1 = MRIDataset(train_img_T1, [], args.resize)
+    train_dataset_T2 = MRIDataset(train_img_T2, [], args.resize)
+    train_dataset_FA = MRIDataset(train_img_FA, []], args.resize)
+    train_dataset_MD = MRIDataset(train_img_MD, [], args.resize)
+    train_dataset_AD = MRIDataset(train_img_AD, [], args.resize)
+    train_dataset_RD = MRIDataset(train_img_RD, [], args.resize)
 
-    print('Mean: {}\n Std: {}\n'.format(mean, std))
+    train_loader_T1 = torch.utils.data.DataLoader(train_dataset_T1, batch_size=args.train_batch_size)
+    train_loader_T2 = torch.utils.data.DataLoader(train_dataset_T2, batch_size=args.train_batch_size)
+    train_loader_FA = torch.utils.data.DataLoader(train_dataset_FA, batch_size=args.train_batch_size)
+    train_loader_MD = torch.utils.data.DataLoader(train_dataset_MD, batch_size=args.train_batch_size)
+    train_loader_AD = torch.utils.data.DataLoader(train_dataset_AD, batch_size=args.train_batch_size)
+    train_loader_RD = torch.utils.data.DataLoader(train_dataset_RD, batch_size=args.train_batch_size)
+
+    means = [0.0] * 6
+    dims = [0.0] * 6
+    vars = [0.0] * 6
+    stds = [0.0] * 6
+
+    train_loader_list = [train_loader_T1, train_loader_T2, train_loader_FA, train_loader_MD, train_loader_AD, train_loader_RD]
+
+    for i in range(len(train_loader_list)):
+        print('Processing {}\n'.format(i))
+        is_first = True
+        train_loader = train_loader_list[i]
+        for batch_img, _ in train_loader:
+            if is_first:
+                img_shape = batch_img[0].shape
+                dims[i] = img_shape[0] * img_shape[1] * img_shape[2]
+                is_first = False
+            means[i] += np.sum(batch_img)
+        
+        means[i] /= (len(train_loader.dataset) * dims[i])
+        
+        for batch_img, _ in train_loader:
+            vars[i] += np.sum(np.power(np.subtract(batch_img, means[i]), 2))
+        
+        vars[i] /= (len(train_loader.dataset) * dims[i])
+        stds[i] = np.sqrt(vars[i])
+    
+    np.save('means.npy', means)
+    np.save('stds.npy', stds)
+    

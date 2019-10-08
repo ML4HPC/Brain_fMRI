b1483ca57958758f8910041106a789917ee07ae3
Seungwook Han <sh3264@columbia.edu>
2019-10-07 21:18:22 -0400

    Checking data for dims

--- check_data.py
+++ check_data.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import argparse
+import IPython
 
 
 def check_count_nan(path):
@@ -23,11 +24,57 @@ def check_count_nan(path):
 
     return nan_count, nan_ratio
 
+def check_dim(path, mri_type):
+    correct_dim = None
+    if mri_type == 'T1' or mri_type == 'T2':
+        correct_dim = 256
+    else:
+        correct_dim = 190
+    
+    train_images = np.load(os.path.join(path, 'train_data_img_{}.npy'.format(mri_type)), allow_pickle=True)
+    train_subjects = np.load(os.path.join(path, 'train_subjects.npy'), allow_pickle=True)
+
+    valid_images = np.load(os.path.join(path, 'valid_data_img_{}.npy'.format(mri_type)), allow_pickle=True)
+    valid_subjects = np.load(os.path.join(path, 'valid_subjects.npy'), allow_pickle=True)
+
+    test_images = np.load(os.path.join(path, 'test_data_img_{}.npy'.format(mri_type)), allow_pickle=True)
+    test_subjects = np.load(os.path.join(path, 'test_subjects.npy'), allow_pickle=True)
+
+    train_err = []
+    valid_err = []
+    test_err = []
+
+    for i in range(len(train_images)):
+        x_shape = np.array(train_images[i].dataobj).astype(float, copy=False).shape()
+
+        if x_shape[0] != correct_dim or x_shape[1] != correct_dim or x_shape[2] != correct_dim:
+            train_err.append(train_subjects[i])
+    
+    for i in range(len(valid_images)):
+        x_shape = np.array(valid_images[i].dataobj).astype(float, copy=False).shape()
+
+        if x_shape[0] != correct_dim or x_shape[1] != correct_dim or x_shape[2] != correct_dim:
+            valid_err.append(valid_subjects[i])
+    
+    for i in range(len(test_images)):
+        x_shape = np.array(test_images[i].dataobj).astype(float, copy=False).shape()
+
+        if x_shape[0] != correct_dim or x_shape[1] != correct_dim or x_shape[2] != correct_dim:
+            test_err.append(test_subjects[i])
+    
+
+    return train_err, valid_err, test_err
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check DTI data for NaN')
     parser.add_argument('--data_dir', help='Path to dataset images')
+    parser.add_argument('--mri_type', default=None, help='MRI Type')
     args = parser.parse_args()
 
-    nan_count, nan_ratio = check_count_nan(args.data_dir)
+    # nan_count, nan_ratio = check_count_nan(args.data_dir)
+    train_err, valid_err, test_err = check_dim(args.data_dir, args.mri_type)
+    IPython.embed()
+
     

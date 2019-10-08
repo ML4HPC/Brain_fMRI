baf30fba2761d8bda4a76143ca6dfef4415b4b97
Seungwook Han <sh3264@columbia.edu>
2019-10-08 17:34:38 +0000

    Added norm option

--- run_6_input_multi_output_resnet3d.sh
+++ run_6_input_multi_output_resnet3d.sh
@@ -9,6 +9,7 @@ fi
 train_bsz=3
 valid_bsz=6
 epoch=10
+normalize=True
 lr=0.001
 momentum=0.5
 weight_decay=0.001
@@ -16,7 +17,7 @@ outdir=$1
 cwd=$(pwd)
 cur_filepath="${cwd}/${0}"
 rm -rf $outdir
-python3 run_6_input_multi_output_resnet3d.py --data_dir=../data/processed/ --output_dir=$outdir --train_batch_size=$train_bsz --valid_batch_size=$valid_bsz --epoch=$epoch --optimizer=sgd --lr=$lr --momentum=$momentum --weight_decay=$weight_decay
+python3 run_6_input_multi_output_resnet3d.py --data_dir=../data/processed/ --output_dir=$outdir --train_batch_size=$train_bsz --valid_batch_size=$valid_bsz --epoch=$epoch --normalize=$normalize --optimizer=sgd --lr=$lr --momentum=$momentum --weight_decay=$weight_decay
 cp $cur_filepath $outdir 
 
 

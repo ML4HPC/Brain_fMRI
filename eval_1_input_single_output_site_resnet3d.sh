e639be99ab10ad4baaf271353fef69901fce96c1
Seungwook Han <sh3264@columbia.edu>
2019-10-22 19:13:29 -0400

    Added site param

--- eval_1_input_single_output_site_resnet3d.sh
+++ eval_1_input_single_output_site_resnet3d.sh
@@ -12,10 +12,11 @@ outdir=$1
 mri_type=$2
 saved_state=$3
 target_idx=11
+site=[16]
 metric=MSE
 cwd=$(pwd)
 cur_filepath="${cwd}/${0}"
-python3 eval_1_input_single_output_resnet3d.py --data_dir=../data/processed/ --output_dir=$outdir --saved_state=$saved_state --valid_batch_size=$valid_bsz --normalize=$normalize --mri_type=$mri_type --target_idx=$target_idx --metric=$metric
+python3 eval_1_input_single_output_resnet3d.py --data_dir=../data/processed/ --output_dir=$outdir --saved_state=$saved_state --valid_batch_size=$valid_bsz --normalize=$normalize --mri_type=$mri_type --target_idx=$target_idx --metric=$metric --site=$site
 cp $cur_filepath $outdir 
 
 

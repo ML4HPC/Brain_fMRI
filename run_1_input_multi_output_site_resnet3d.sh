89ed34bb35a6c6138c07d53f0ae102f697007f56
Seungwook Han <sh3264@columbia.edu>
2019-10-22 21:48:12 +0000

    Removed target idx arg

--- run_1_input_multi_output_site_resnet3d.sh
+++ run_1_input_multi_output_site_resnet3d.sh
@@ -19,7 +19,7 @@ site=[16]
 cwd=$(pwd)
 cur_filepath="${cwd}/${0}"
 rm -rf $outdir
-python3 run_1_input_multi_output_site_resnet3d.py --data_dir=../data/processed_norm/ --output_dir=$outdir --train_batch_size=$train_bsz --valid_batch_size=$valid_bsz --epoch=$epoch --normalize=$normalize --optimizer=$optim --lr=$lr --weight_decay=$weight_decay --mri_type=$mri_type --target_idx=$target_idx --site=$site
+python3 run_1_input_multi_output_site_resnet3d.py --data_dir=../data/processed_norm/ --output_dir=$outdir --train_batch_size=$train_bsz --valid_batch_size=$valid_bsz --epoch=$epoch --normalize=$normalize --optimizer=$optim --lr=$lr --weight_decay=$weight_decay --mri_type=$mri_type --site=$site
 cp $cur_filepath $outdir 
 
 

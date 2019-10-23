Fixing log softmax

metrics.py
@@ -4,8 +4,9 @@ from sklearn.metrics import classification_report
 
 def outputs_multi_classification_report(y_true, y_preds):
     y_pred_labels = []
+    lsm = torch.nn.LogSoftmax()
     for pred in y_preds:
-        sm_pred = F.log_softmax(pred, dim=1)
+        sm_pred = lsm(pred)
         pred_label = sm_pred.max(1, keepdim=True)[1]
         y_pred_labels.append(pred_label)
     
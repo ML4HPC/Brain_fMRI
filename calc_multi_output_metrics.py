53f94d538bca3b85870e4d946c01cde49dc1d07e
Seungwook Han <sh3264@columbia.edu>
2019-10-22 23:43:55 -0400

    Converting from np to torch

--- calc_multi_output_metrics.py
+++ calc_multi_output_metrics.py
@@ -1,10 +1,5 @@
 import torch
-import torch.optim as optim
-import torch.nn as nn
-import multi_input_resnet3d 
 import numpy as np
-from mri_dataset import MultiMRIDataset
-from model_3d_multi import train_multi, eval_multi
 from metrics import outputs_multi_classification_report, outputs_bin_classification_report
 from sklearn.metrics import r2_score, mean_squared_error
 import argparse
@@ -35,8 +30,8 @@ if __name__ == "__main__":
     for i in range(len(valid_target_pred)):
         print('Processing class {}/21'.format(i+1))
         score = None
-        y_true = valid_target_true[i]
-        y_pred = valid_target_pred[i]
+        y_true = torch.from_numpy(valid_target_true[i])
+        y_pred = torch.from_numpy(valid_target_pred[i])
 
         if i in bin_idx:
             score = outputs_bin_classification_report(y_true, y_pred)

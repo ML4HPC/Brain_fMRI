3e3ca6c957beb91d5742eae6cc434fd423e90b84
Seungwook Han <sh3264@columbia.edu>
2019-10-23 12:02:24 -0400

    Added moving mean

--- draw_loss_plot.py
+++ draw_loss_plot.py
@@ -2,15 +2,20 @@ import os
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import bottleneck as bn
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Draw train loss plot')
     parser.add_argument('--data_dir', help='Directory path for train loss data')
     parser.add_argument('--lr', help='LR of the model')
+    parser.add_argument('--move_mean', type=int, default=0, help='Whether to compute moving mean or not')
     args = parser.parse_args()
 
     train_loss = np.load(os.path.join(args.data_dir, 'loss_history_train.npy'), allow_pickle=True)
+    
+    if args.move_mean:
+        train_loss = bn.move_mean(t, window=5, min_count=1)
 
     plt.scatter(range(len(train_loss)), train_loss, color='black', s=1)
     plt.xlabel('Iterations')

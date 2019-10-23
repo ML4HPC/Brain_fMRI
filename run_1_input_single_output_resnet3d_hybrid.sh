a737b73a80bd44208da686b4576c2a18d4e6bd24
Yan Zhang <yzhanglearning>
2019-10-23 00:32:11 -0700

    hybrid parallel run file

---
+++ run_1_input_single_output_resnet3d_hybrid.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]
+then
+    echo 'Provide output directory as command line argument!'
+    exit 1
+fi
+
+train_bsz=3
+valid_bsz=10
+epoch=10
+normalize=True
+optim=adam
+lr=0.001
+weight_decay=0
+outdir=$1
+mri_type=$2
+target_idx=0
+cwd=$(pwd)
+cur_filepath="${cwd}/${0}"
+rm -rf $outdir
+python3 run_1_input_single_output_resnet3d.py --data_dir=../data/processed/ --output_dir=$outdir --train_batch_size=$train_bsz --valid_batch_size=$valid_bsz --epoch=$epoch --normalize=$normalize --optimizer=$optim --lr=$lr --weight_decay=$weight_decay --mri_type=$mri_type --target_idx=$target_idx
+cp $cur_filepath $outdir 
+
+
